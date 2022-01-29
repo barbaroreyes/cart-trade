93d2f319f9938a919934f6d03f6973e4627696dc
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2022-01-29 13:09:09 -0600

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Layout from '../compo/layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <Layout>
+     <Component {...pageProps}  />
+     </Layout>
+    
+ 
+  )
 }
 
 export default MyApp
